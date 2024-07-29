45749e336a199976dff3364369a989477bd9225e
malek <malek.dhaouadi@esprit.tn>
2024-07-30 00:57:06 +0100

    initial commit of snake game in java

---
+++ app.java
@@ -0,0 +1,28 @@
+
+import javax.swing.JFrame;
+
+
+public class app {
+    public static void main(String[] args) throws Exception{
+        int boardWidth = 600;
+        int boardHeight = boardWidth;
+
+        JFrame frame = new JFrame("Snake");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        SnakeGame snakeGame= new SnakeGame(boardWidth, boardHeight);
+        frame.add(snakeGame);
+        frame.pack();
+        snakeGame.requestFocus();
+        
+
+
+        
+        
+    }
+    
+}
